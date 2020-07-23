51ac35d3e7e014007e969bcfc3467df0f4f16c3b
Ubuntu <ubuntu@ip-10-0-0-156.ec2.internal>
2020-07-23 14:19:01 +0000

    cloud formation script

---
+++ createcluster.sh
@@ -0,0 +1,1 @@
+eksctl create cluster --name capstonecluster --version 1.17 --nodegroup-name standard-workers --node-type t3.medium --nodes 2 --nodes-min 1 --nodes-max 3 --node-ami auto --region us-east-1 --zones us-east-1a --zones us-east-1b --zones us-east-1c
